fca25ac8b87b7ebe2a926d952a336d26c503015a
ShotaroTsutsumi <shotaro71@gmail.com>
2020-05-10 20:37:54 +0900

    message-automatic-updating-function

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,46 +1,75 @@
-$(function(){
-     function buildHTML(message){
-      if ( message.image ) {
-        var html =
-        `<div class="message">
-            <div class="upper-info">
-              <div class="upper-info__talker">
-                ${message.user_name}
-              </div>
-              <div class="upper-info__date">
-                <${message.created_at}
-              </div>
+$(function() {
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
             </div>
-            <div class="lower-info">
-              <p class="lower-info__content">
-                ${message.content}
-              </p>
+            <div class="upper-info__date">
+              <${message.created_at}
             </div>
-            <img src=${message.image} >
-         </div>`
-       return html;
-      } else {
-        var html =
-         `<div class="message">
-            <div class="upper-info">
-              <div class="upper-info__talker">
-                ${message.user_name}
-              </div>
-              <div class="upper-info__date">
-                ${message.created_at}
-              </div>
+          </div>
+          <div class="lower-info">
+            <p class="lower-info__content">
+              ${message.content}
+            </p>
+          </div>
+          <img class = "lower-info__image" src="${message.image}" >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
             </div>
-            <div class="lower-info">
-              <p class="lower-info__content">
-                ${message.content}
-              </p>
+            <div class="upper-info__date">
+              ${message.created_at}
             </div>
-          </div>`
-        return html;
-      };
-    }
+          </div>
+          <div class="lower-info">
+            <p class="lower-info__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
      
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -62,5 +91,5 @@ $('#new_message').on('submit', function(e){
     .fail(function() {
          alert("メッセージ送信に失敗しました");
     });
-})
+  })
 });
